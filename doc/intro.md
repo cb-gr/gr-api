281c401874ae5dace3b4a03858b4afb2358e15f6
Chris Bui <christopher.d.bui@gmail.com>
2017-04-06 11:59:04 -0500

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gr-parser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gr_parser/core_test.clj
@@ -0,0 +1,7 @@
+(ns gr-parser.core-test
+  (:require [clojure.test :refer :all]
+            [gr-parser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
