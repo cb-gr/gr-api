795fa5257d4a1a478404a149bd7a489014f048f8
Chris Bui <christopher.d.bui@gmail.com>
2017-04-09 11:52:48 -0500

    Add custom constructor for person
    This parses dates into an internal representation and allows future
    parsing/transformations to be done prior to creation of record.

--- src/gr_parser/core.clj
+++ src/gr_parser/core.clj
@@ -2,6 +2,7 @@
   (:require [clojure.string :as str]))
 
 (def delimitters
+(def ^:const delimitters
   "A map of the record's type of delimitter to a regex to split the
   record"
   {:pipe  #" \| "
@@ -11,11 +12,23 @@
 (def fields [:first-name :last-name :gender :favorite-color :date-of-birth])
 (defrecord Person [first-name last-name gender favorite-color date-of-birth])
 
+(def date-of-birth-format (java.text.SimpleDateFormat. "MM/dd/yyyy"))
+
+(defn parse-date-of-birth
+  [dob]
+  (.parse date-of-birth-format dob))
+
+(defn person
+  "Constructs a Person with date-of-birth parsed from a map"
+  [p]
+  (-> p
+      (update :date-of-birth parse-date-of-birth)
+      (map->Person)))
+
 (defn parse-line
   [delimitter-regex line]
   (->> (str/split line delimitter-regex)
-       (map hash-map fields)
-       (reduce merge)
-       (map->Person)))
+       (zipmap fields)
+       (person)))
 
 
