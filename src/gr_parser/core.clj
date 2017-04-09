8b09c3c2aeba0166dcb9107c29e0627416d331ac
Chris Bui <christopher.d.bui@gmail.com>
2017-04-09 11:14:05 -0500

    Add parse-line fn and Person record

--- src/gr_parser/core.clj
+++ src/gr_parser/core.clj
@@ -1,6 +1,21 @@
-(ns gr-parser.core)
+(ns gr-parser.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def delimitters
+  "A map of the record's type of delimitter to a regex to split the
+  record"
+  {:pipe  #" \| "
+   :comma #", "
+   :space #" "})
+
+(def fields [:first-name :last-name :gender :favorite-color :date-of-birth])
+(defrecord Person [first-name last-name gender favorite-color date-of-birth])
+
+(defn parse-line
+  [delimitter-regex line]
+  (->> (str/split line delimitter-regex)
+       (map hash-map fields)
+       (reduce merge)
+       (map->Person)))
+
+
--- test/gr_parser/core_test.clj
+++ test/gr_parser/core_test.clj
@@ -2,6 +2,23 @@
   (:require [clojure.test :refer :all]
             [gr-parser.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def expected-record (map->Person {:last-name      "FirstName"
+                                   :first-name     "LastName"
+                                   :date-of-birth  "DateOfBirth"
+                                   :favorite-color "FavoriteColor"
+                                   :gender         "Gender"}))
+
+(deftest parse-line-pipe-test
+  (is (= expected-record
+         (parse-line (:pipe delimitters)
+                     "LastName | FirstName | Gender | FavoriteColor | DateOfBirth"))))
+
+(deftest parse-line-comma-test
+  (is (= expected-record
+         (parse-line (:comma delimitters)
+                     "LastName, FirstName, Gender, FavoriteColor, DateOfBirth"))))
+
+(deftest parse-line-space-test
+  (is (= expected-record
+         (parse-line (:space delimitters)
+                     "LastName FirstName Gender FavoriteColor DateOfBirth"))))
