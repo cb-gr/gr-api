3973c66684ba118ae250bf6b3246e4126274a7bb
Chris Bui <christopher.d.bui@gmail.com>
2017-04-09 21:15:18 -0500

    Add middleware for json

--- project.clj
+++ project.clj
@@ -9,4 +9,5 @@
                  [ring/ring-core "1.5.0"]
                  [ring/ring-defaults "0.2.3"]
                  [ring/ring-jetty-adapter "1.5.0"]
+                 [ring/ring-json "0.4.0"]
                  [ring/ring-mock "0.3.0"]])
